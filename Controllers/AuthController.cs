06189d293c654e5cdea807cce30d36bd5a64d50e
Visanu <mvisanu@gmail.com>
2022-07-12 13:51:24 -0500

    latest update

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -54,7 +54,8 @@ namespace TACApp2Api.Controllers
         {
             List<Claim> claims = new List<Claim>
             {
-                new Claim(ClaimTypes.Name, user.Username)
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
